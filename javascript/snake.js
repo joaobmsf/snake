18c77936afddf2c17252c2bdb6a36fe59b71860b
joaobmsf <joao.batista665@gmail.com>
2014-05-09 14:50:31 -0300

    Teste para criacao do grid.

--- javascript/snake.js
+++ javascript/snake.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-  
+  for(var i = 0; i < 10; ++i) {
+    $(".tela").append('<div class="row"> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div class="cell js-cell"> &nbsp; </div> \
+      <div style="clear: both;"></div> \
+    </div>');
+  }
+
+  var jsCells = $(".js-cell");
+  var width = jsCells.css("width");
+  jsCells.css("height", width);
+
+  jsCells.hide();
+  jsCells.fadeIn("slow");
+
 });
+
